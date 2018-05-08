6544e8c40eccf0c8e674b2bb45852001dc45e0fd
Yash Dwivedi <34545149+Yazdwivedi@users.noreply.github.com>
2018-05-08 15:50:49 +0530

    Update models.py

--- revisionfinal/app/models.py
+++ revisionfinal/app/models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class users(models.Model):
-    user=models.OneToOneField(User)
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
     portfolio=models.URLField(blank=True)
     image=models.ImageField(upload_to="profile_pics",blank=True)
